ce928cea5bf73f0c090b0cc10e5c6663c5bafed5
andraspreisler <preisler.andras@gmail.com>
2026-02-22 16:14:47 +0100

    Initial Commit

---
+++ shelfappTests/shelfappTests.swift
@@ -0,0 +1,17 @@
+//
+//  shelfappTests.swift
+//  shelfappTests
+//
+//  Created by Andras Preisler on 2026. 02. 22..
+//
+
+import Testing
+@testable import shelfapp
+
+struct shelfappTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
