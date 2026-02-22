78abf4cc6335775850abe5f93991e7850c475344
andraspreisler <preisler.andras@gmail.com>
2026-02-22 13:30:00 +0100

    feat: add initial view components (ContentView, StorageViews)

---
+++ shelfapp/Features/Views/ContentView.swift
@@ -0,0 +1,5 @@
+// This file has been moved to Features/Pages/RootView.swift
+// Please use RootView instead.
+
+import SwiftUI
+
---
+++ shelfapp/Features/Views/StorageViews.swift
@@ -0,0 +1,5 @@
+// This file contains the StorageDetailView which is now in Features/Pages/StoragesView.swift
+// Please use the Pages folder for all main views.
+
+import SwiftUI
+
