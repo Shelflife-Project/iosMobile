67152b42c3419576582ffddbedc555bfc186bf2b
andraspreisler <preisler.andras@gmail.com>
2026-03-12 09:21:31 +0100

    feat: add AnimatedSecureTextField view with basic structure

---
+++ shelfapp/Features/Views/AnimatedSecureTextField.swift
@@ -0,0 +1,18 @@
+//
+//  AnimatedSecureTextField.swift
+//  shelfapp
+//
+//  Created by Andras Preisler on 2026. 03. 10..
+//
+
+import SwiftUI
+
+struct AnimatedSecureTextField: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AnimatedSecureTextField()
+}
