73a85d1a8a8291e6d48da25c9f6fb24ea1c51ffe
andraspreisler <preisler.andras@gmail.com>
2026-04-18 19:10:03 +0200

    mobile(profile): split api protocol and default implementation

---
+++ shelfapp/Domains/Profile/Utils/API/DefaultProfileAPI.swift
@@ -0,0 +1,22 @@
+import Foundation
+
+struct DefaultProfileAPI: ProfileAPI {
+    private let http: HTTPClient
+
+    init(http: HTTPClient) {
+        self.http = http
+    }
+
+    init() {
+        self.init(http: DefaultHTTPClient())
+    }
+
+    func updateUser(id: Int, username: String?, email: String?) async throws -> User {
+        let dto: UserAuthDTO = try await http.request(.updateUser(id: id, body: UserUpdateBody(username: username, email: email)))
+        return dto.toDomain()
+    }
+
+    func uploadUserProfilePicture(userId: Int, imageData: Data) async throws {
+        let _: EmptyResponse = try await http.request(.uploadUserProfilePicture(userId: userId, imageData: imageData))
+    }
+}
--- shelfapp/Domains/Profile/Utils/API/ProfileAPI.swift
+++ shelfapp/Domains/Profile/Utils/API/ProfileAPI.swift
@@ -4,24 +4,3 @@ protocol ProfileAPI {
     func updateUser(id: Int, username: String?, email: String?) async throws -> User
     func uploadUserProfilePicture(userId: Int, imageData: Data) async throws
 }
-
-struct DefaultProfileAPI: ProfileAPI {
-    private let http: HTTPClient
-
-    init(http: HTTPClient) {
-        self.http = http
-    }
-
-    init() {
-        self.init(http: DefaultHTTPClient())
-    }
-
-    func updateUser(id: Int, username: String?, email: String?) async throws -> User {
-        let dto: UserAuthDTO = try await http.request(.updateUser(id: id, body: UserUpdateBody(username: username, email: email)))
-        return dto.toDomain()
-    }
-
-    func uploadUserProfilePicture(userId: Int, imageData: Data) async throws {
-        let _: EmptyResponse = try await http.request(.uploadUserProfilePicture(userId: userId, imageData: imageData))
-    }
-}
